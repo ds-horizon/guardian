e47e434c270c7472102f3d6a10fa55fa2b4e93b3
pranavreddyd11 <pranav.reddy@dream11.com>
2025-07-03 14:24:59 +0530

    REFACTORED CONFIG QUERY

--- src/main/java/com/dreamsportslabs/guardian/dao/query/ConfigQuery.java
+++ src/main/java/com/dreamsportslabs/guardian/dao/query/ConfigQuery.java
@@ -109,22 +109,22 @@ public class ConfigQuery {
 
   public static final String OIDC_CONFIG =
       """
-            SELECT tenant_id,
-                   issuer,
-                   authorization_endpoint,
-                   token_endpoint,
-                   userinfo_endpoint,
-                   revocation_endpoint,
-                   jwks_uri,
-                   login_page_uri,
-                   consent_page_uri,
-                   authorize_ttl,
-                   grant_types_supported,
-                   response_types_supported,
-                   subject_types_supported,
-                   id_token_signing_alg_values_supported,
-                   token_endpoint_auth_methods_supported
-            FROM oidc_config
-            WHERE tenant_id = ?
-            """;
+    SELECT tenant_id,
+           issuer,
+           authorization_endpoint,
+           token_endpoint,
+           userinfo_endpoint,
+           revocation_endpoint,
+           jwks_uri,
+           login_page_uri,
+           consent_page_uri,
+           authorize_ttl,
+           grant_types_supported,
+           response_types_supported,
+           subject_types_supported,
+           id_token_signing_alg_values_supported,
+           token_endpoint_auth_methods_supported
+    FROM oidc_config
+    WHERE tenant_id = ?
+    """;
 }
