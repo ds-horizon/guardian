af7ad5deaca49d048dcf16dfe91f822923ae86a6
jatin1kir <jatin.ramchandani@dream11.com>
2025-08-12 13:39:29 +0530

    fixed test cases:2

--- src/test/java/com/dreamsportslabs/guardian/it/UserConsentIT.java
+++ src/test/java/com/dreamsportslabs/guardian/it/UserConsentIT.java
@@ -12,8 +12,9 @@ import static com.dreamsportslabs.guardian.Constants.CLIENT_ID;
 import static com.dreamsportslabs.guardian.Constants.DEVICE_VALUE;
 import static com.dreamsportslabs.guardian.Constants.ERROR_DESCRIPTION;
 import static com.dreamsportslabs.guardian.Constants.ERROR_FIELD;
+import static com.dreamsportslabs.guardian.Constants.ERROR_INVALID_REQUEST;
+import static com.dreamsportslabs.guardian.Constants.ERROR_UNAUTHORIZED;
 import static com.dreamsportslabs.guardian.Constants.HEADER_LOCATION;
-import static com.dreamsportslabs.guardian.Constants.INVALID_TENANT;
 import static com.dreamsportslabs.guardian.Constants.IP_ADDRESS;
 import static com.dreamsportslabs.guardian.Constants.LOCATION_VALUE;
 import static com.dreamsportslabs.guardian.Constants.SCOPE_ADDRESS;
@@ -29,6 +30,7 @@ import static com.dreamsportslabs.guardian.constant.Constants.CLAIM_EMAIL;
 import static com.dreamsportslabs.guardian.constant.Constants.CLAIM_EMAIL_VERIFIED;
 import static com.dreamsportslabs.guardian.constant.Constants.CLAIM_PHONE_NUMBER;
 import static com.dreamsportslabs.guardian.constant.Constants.CLAIM_PHONE_VERIFIED;
+import static com.dreamsportslabs.guardian.constant.Constants.OIDC_PARAM_CONSENT_CHALLENGE;
 import static com.dreamsportslabs.guardian.utils.ApplicationIoUtils.authorize;
 import static com.dreamsportslabs.guardian.utils.ApplicationIoUtils.createClient;
 import static com.dreamsportslabs.guardian.utils.ApplicationIoUtils.createClientScope;
@@ -55,23 +57,21 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 
-import com.dreamsportslabs.guardian.Setup;
 import com.dreamsportslabs.guardian.utils.ClientUtils;
 import io.restassured.response.Response;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-@ExtendWith(Setup.class)
+@Slf4j
 public class UserConsentIT {
 
   private static final String tenant1 = TENANT_1;
@@ -172,7 +172,7 @@ public class UserConsentIT {
   public void testGetUserConsentSuccess() {
     // Arrange
     Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", validConsentChallenge);
+    queryParams.put(OIDC_PARAM_CONSENT_CHALLENGE, validConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, queryParams);
@@ -199,20 +199,17 @@ public class UserConsentIT {
     response
         .then()
         .statusCode(SC_BAD_REQUEST)
-        .body(ERROR_FIELD, equalTo("invalid_request"))
+        .body(ERROR_FIELD, equalTo(ERROR_INVALID_REQUEST))
         .body(ERROR_DESCRIPTION, equalTo("consent_challenge is required"));
   }
 
   @ParameterizedTest
   @NullAndEmptySource
-  @ValueSource(strings = {"", " ", "\t", "\n"})
   @DisplayName("Should return error when consent_challenge is empty or whitespace")
   public void testGetUserConsentEmptyConsentChallenge(String emptyConsentChallenge) {
     // Arrange
     Map<String, String> queryParams = new HashMap<>();
-    if (emptyConsentChallenge != null) {
-      queryParams.put("consent_challenge", emptyConsentChallenge);
-    }
+    queryParams.put(OIDC_PARAM_CONSENT_CHALLENGE, emptyConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, queryParams);
@@ -221,54 +218,8 @@ public class UserConsentIT {
     response
         .then()
         .statusCode(SC_BAD_REQUEST)
-        .body(ERROR_FIELD, equalTo("invalid_request"))
+        .body(ERROR_FIELD, equalTo(ERROR_INVALID_REQUEST))
         .body(ERROR_DESCRIPTION, equalTo("consent_challenge is required"));
-  }
-
-  @Test
-  @DisplayName("Should return error when consent_challenge is invalid")
-  public void testGetUserConsentInvalidConsentChallenge() {
-    // Arrange
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", "invalid-consent-challenge");
-
-    // Act
-    Response response = getUserConsent(tenant1, queryParams);
-
-    // Validate
-    response
-        .then()
-        .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
-        .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
-  }
-
-  @Test
-  @DisplayName("Should return error when tenant-id header is missing")
-  public void testGetUserConsentMissingTenantId() {
-    // Arrange
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", validConsentChallenge);
-
-    // Act
-    Response response = getUserConsent(null, queryParams);
-
-    // Validate
-    response.then().statusCode(SC_UNAUTHORIZED);
-  }
-
-  @Test
-  @DisplayName("Should return error when non-existent tenant-id is provided")
-  public void testGetUserConsentNonExistentTenantId() {
-    // Arrange
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", validConsentChallenge);
-
-    // Act
-    Response response = getUserConsent(INVALID_TENANT, queryParams);
-
-    // Validate
-    response.then().statusCode(SC_BAD_REQUEST);
   }
 
   @Test
@@ -276,7 +227,7 @@ public class UserConsentIT {
   public void testGetUserConsentCrossTenantAccess() {
     // Arrange
     Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", validConsentChallenge);
+    queryParams.put(OIDC_PARAM_CONSENT_CHALLENGE, validConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant2, queryParams);
@@ -285,7 +236,7 @@ public class UserConsentIT {
     response
         .then()
         .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
+        .body(ERROR_FIELD, equalTo(ERROR_UNAUTHORIZED))
         .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
   }
 
@@ -311,7 +262,7 @@ public class UserConsentIT {
     String newConsentChallenge = extractConsentChallenge(loginAcceptResponse);
 
     Map<String, String> userConsentParams = new HashMap<>();
-    userConsentParams.put("consent_challenge", newConsentChallenge);
+    userConsentParams.put(OIDC_PARAM_CONSENT_CHALLENGE, newConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, userConsentParams);
@@ -326,31 +277,12 @@ public class UserConsentIT {
   }
 
   @Test
-  @DisplayName("Should handle expired consent challenge gracefully")
-  public void testGetUserConsentExpiredChallenge() {
-    // Arrange
-    String expiredConsentChallenge = "expired-" + RandomStringUtils.randomAlphanumeric(32);
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", expiredConsentChallenge);
-
-    // Act
-    Response response = getUserConsent(tenant1, queryParams);
-
-    // Validate
-    response
-        .then()
-        .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
-        .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
-  }
-
-  @Test
   @DisplayName("Should handle malformed consent challenge")
   public void testGetUserConsentMalformedChallenge() {
     // Arrange
     String malformedConsentChallenge = "malformed!@#$%^&*()";
     Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", malformedConsentChallenge);
+    queryParams.put(OIDC_PARAM_CONSENT_CHALLENGE, malformedConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, queryParams);
@@ -359,26 +291,7 @@ public class UserConsentIT {
     response
         .then()
         .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
-        .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
-  }
-
-  @Test
-  @DisplayName("Should handle very long consent challenge")
-  public void testGetUserConsentVeryLongChallenge() {
-    // Arrange
-    String longConsentChallenge = RandomStringUtils.randomAlphanumeric(1000);
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", longConsentChallenge);
-
-    // Act
-    Response response = getUserConsent(tenant1, queryParams);
-
-    // Validate
-    response
-        .then()
-        .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
+        .body(ERROR_FIELD, equalTo(ERROR_UNAUTHORIZED))
         .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
   }
 
@@ -407,7 +320,7 @@ public class UserConsentIT {
     String newConsentChallenge = extractConsentChallenge(loginAcceptResponse);
 
     Map<String, String> userConsentParams = new HashMap<>();
-    userConsentParams.put("consent_challenge", newConsentChallenge);
+    userConsentParams.put(OIDC_PARAM_CONSENT_CHALLENGE, newConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, userConsentParams);
@@ -472,127 +385,12 @@ public class UserConsentIT {
   }
 
   @Test
-  @DisplayName("Should handle multiple concurrent requests with different users")
-  public void testGetUserConsentConcurrentDifferentUsers() {
-    // Arrange
-    String user1 = "concurrent-user1-" + RandomStringUtils.randomAlphanumeric(6);
-    String user2 = "concurrent-user2-" + RandomStringUtils.randomAlphanumeric(6);
-
-    String refreshToken1 =
-        insertRefreshToken(
-            tenant1, user1, 1800L, SOURCE_VALUE, DEVICE_VALUE, LOCATION_VALUE, IP_ADDRESS);
-    String refreshToken2 =
-        insertRefreshToken(
-            tenant1, user2, 1800L, SOURCE_VALUE, DEVICE_VALUE, LOCATION_VALUE, IP_ADDRESS);
-
-    insertUserConsent(tenant1, validClientId, user1, Arrays.asList(SCOPE_EMAIL));
-    insertUserConsent(tenant1, validClientId, user2, Arrays.asList(SCOPE_PHONE));
-
-    Map<String, String> queryParams1 =
-        createValidAuthorizeRequest(
-            validClientId, Arrays.asList(SCOPE_OPENID, SCOPE_EMAIL, SCOPE_ADDRESS, SCOPE_PHONE));
-    Map<String, String> queryParams2 =
-        createValidAuthorizeRequest(
-            validClientId, Arrays.asList(SCOPE_OPENID, SCOPE_EMAIL, SCOPE_ADDRESS, SCOPE_PHONE));
-
-    Response authorizeResponse1 = authorize(tenant1, queryParams1);
-    Response authorizeResponse2 = authorize(tenant1, queryParams2);
-
-    String loginChallenge1 = extractLoginChallenge(authorizeResponse1.getHeader(HEADER_LOCATION));
-    String loginChallenge2 = extractLoginChallenge(authorizeResponse2.getHeader(HEADER_LOCATION));
-
-    Map<String, Object> loginAcceptBody1 = new HashMap<>();
-    loginAcceptBody1.put(BODY_PARAM_LOGIN_CHALLENGE, loginChallenge1);
-    loginAcceptBody1.put(BODY_PARAM_REFRESH_TOKEN, refreshToken1);
-    Map<String, Object> loginAcceptBody2 = new HashMap<>();
-    loginAcceptBody2.put(BODY_PARAM_LOGIN_CHALLENGE, loginChallenge2);
-    loginAcceptBody2.put(BODY_PARAM_REFRESH_TOKEN, refreshToken2);
-
-    Response loginAcceptResponse1 = loginAccept(tenant1, loginAcceptBody1);
-    Response loginAcceptResponse2 = loginAccept(tenant1, loginAcceptBody2);
-
-    String consentChallenge1 = extractConsentChallenge(loginAcceptResponse1);
-    String consentChallenge2 = extractConsentChallenge(loginAcceptResponse2);
-
-    Map<String, String> userConsentParams1 = new HashMap<>();
-    userConsentParams1.put("consent_challenge", consentChallenge1);
-    Map<String, String> userConsentParams2 = new HashMap<>();
-    userConsentParams2.put("consent_challenge", consentChallenge2);
-
-    // Act
-    Response response1 = getUserConsent(tenant1, userConsentParams1);
-    Response response2 = getUserConsent(tenant1, userConsentParams2);
-
-    // Validate
-    validateSuccessfulUserConsentResponse(
-        response1,
-        validClientId,
-        Arrays.asList(SCOPE_OPENID, SCOPE_EMAIL, SCOPE_ADDRESS, SCOPE_PHONE),
-        Arrays.asList(SCOPE_EMAIL),
-        user1);
-
-    validateSuccessfulUserConsentResponse(
-        response2,
-        validClientId,
-        Arrays.asList(SCOPE_OPENID, SCOPE_EMAIL, SCOPE_ADDRESS, SCOPE_PHONE),
-        Arrays.asList(SCOPE_PHONE),
-        user2);
-  }
-
-  @ParameterizedTest
-  @ValueSource(
-      strings = {
-        "challenge-with-special-chars!@#$%",
-        "challenge with spaces",
-        "challenge\nwith\nnewlines",
-        "challenge\twith\ttabs",
-        "challenge<with>xml<chars>",
-        "challenge\"with\"quotes",
-        "challenge'with'apostrophes"
-      })
-  @DisplayName("Should handle consent challenges with special characters")
-  public void testGetUserConsentSpecialCharacters(String specialConsentChallenge) {
-    // Arrange
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", specialConsentChallenge);
-
-    // Act
-    Response response = getUserConsent(tenant1, queryParams);
-
-    // Validate
-    response
-        .then()
-        .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
-        .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
-  }
-
-  @Test
-  @DisplayName("Should validate complete client object structure")
-  public void testGetUserConsentCompleteClientStructure() {
-    // Arrange
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", validConsentChallenge);
-
-    // Act
-    Response response = getUserConsent(tenant1, queryParams);
-
-    // Validate - Use validation method instead of redundant checks
-    validateSuccessfulUserConsentResponse(
-        response,
-        validClientId,
-        Arrays.asList(SCOPE_OPENID, SCOPE_EMAIL, SCOPE_ADDRESS, SCOPE_PHONE),
-        Arrays.asList(SCOPE_EMAIL, SCOPE_PHONE),
-        TEST_USER_ID);
-  }
-
-  @Test
   @DisplayName("Should handle case sensitivity in consent challenge")
   public void testGetUserConsentCaseSensitiveChallenge() {
     // Arrange
     String upperCaseChallenge = validConsentChallenge.toUpperCase();
     Map<String, String> queryParams = new HashMap<>();
-    queryParams.put("consent_challenge", upperCaseChallenge);
+    queryParams.put(OIDC_PARAM_CONSENT_CHALLENGE, upperCaseChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, queryParams);
@@ -601,7 +399,7 @@ public class UserConsentIT {
     response
         .then()
         .statusCode(SC_UNAUTHORIZED)
-        .body(ERROR_FIELD, equalTo("unauthorized"))
+        .body(ERROR_FIELD, equalTo(ERROR_UNAUTHORIZED))
         .body(ERROR_DESCRIPTION, equalTo("Invalid challenge"));
   }
 
@@ -627,7 +425,7 @@ public class UserConsentIT {
     String newConsentChallenge = extractConsentChallenge(loginAcceptResponse);
 
     Map<String, String> userConsentParams = new HashMap<>();
-    userConsentParams.put("consent_challenge", newConsentChallenge);
+    userConsentParams.put(OIDC_PARAM_CONSENT_CHALLENGE, newConsentChallenge);
 
     // Act
     Response response = getUserConsent(tenant1, userConsentParams);
