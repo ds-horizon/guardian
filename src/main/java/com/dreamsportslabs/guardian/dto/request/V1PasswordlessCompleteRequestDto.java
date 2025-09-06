cce7c4c5deb1016333950712b41475107e10b233
Pujan Burad <buradpujan11@gmail.com>
2025-09-06 19:48:54 +0530

    fix: handling of additional claims

--- src/main/java/com/dreamsportslabs/guardian/dto/request/V1PasswordlessCompleteRequestDto.java
+++ src/main/java/com/dreamsportslabs/guardian/dto/request/V1PasswordlessCompleteRequestDto.java
@@ -2,7 +2,11 @@ package com.dreamsportslabs.guardian.dto.request;
 
 import static com.dreamsportslabs.guardian.exception.ErrorEnum.INVALID_REQUEST;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.Map;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,9 +21,21 @@ public class V1PasswordlessCompleteRequestDto {
   private String state;
   private String otp;
 
+  @JsonIgnore private Map<String, Object> additionalInfo;
+
   public void validate() {
     if (state == null) {
       throw INVALID_REQUEST.getCustomException("Invalid state");
     }
+  }
+
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalInfo() {
+    return additionalInfo;
+  }
+
+  @JsonAnySetter
+  public void addAdditionalInfo(String name, Object value) {
+    additionalInfo.put(name, value);
   }
 }
