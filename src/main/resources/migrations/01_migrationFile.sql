9ac62933c6ffe5ee2979570f3b459416d71d66df
pranavreddyd11 <pranav.reddy@dream11.com>
2025-09-09 15:03:51 +0530

    fix:modify guestConfig migrations, to add case sensitivity for secret_key (#76)

--- src/main/resources/migrations/01_migrationFile.sql
+++ src/main/resources/migrations/01_migrationFile.sql
@@ -82,12 +82,12 @@ CREATE TABLE sms_config
 
 CREATE TABLE fb_config
 (
-    tenant_id           CHAR(10)     PRIMARY KEY,
-    app_id              VARCHAR(256) NOT NULL,
-    app_secret          VARCHAR(256) NOT NULL,
-    send_app_secret     BOOLEAN      NOT NULL DEFAULT TRUE,
-    created_at          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    tenant_id       CHAR(10) PRIMARY KEY,
+    app_id          VARCHAR(256) NOT NULL,
+    app_secret      VARCHAR(256) NOT NULL,
+    send_app_secret BOOLEAN      NOT NULL DEFAULT TRUE,
+    created_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_tenant_fb_config FOREIGN KEY (tenant_id)
         REFERENCES tenant (id) ON DELETE CASCADE,
@@ -117,22 +117,22 @@ CREATE TABLE google_config
 
 CREATE TABLE token_config
 (
-    tenant_id                 CHAR(10) PRIMARY KEY,
-    algorithm                 VARCHAR(10)  NOT NULL,
-    issuer                    VARCHAR(256) NOT NULL,
-    rsa_keys                  JSON         NOT NULL,
-    access_token_expiry       INT          NOT NULL,
-    refresh_token_expiry      INT          NOT NULL,
-    id_token_expiry           INT          NOT NULL,
-    id_token_claims           JSON         NOT NULL,
-    cookie_same_site          VARCHAR(20)  NOT NULL DEFAULT 'LAX',
-    cookie_domain             VARCHAR(256) NOT NULL,
-    cookie_path               VARCHAR(256) NOT NULL DEFAULT '/',
-    cookie_secure             BOOLEAN      NOT NULL DEFAULT FALSE,
-    cookie_http_only          BOOLEAN      NOT NULL DEFAULT TRUE,
-    access_token_claims       JSON         NOT NULL DEFAULT (JSON_ARRAY()),
-    created_at                TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at                TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    tenant_id            CHAR(10) PRIMARY KEY,
+    algorithm            VARCHAR(10)  NOT NULL,
+    issuer               VARCHAR(256) NOT NULL,
+    rsa_keys             JSON         NOT NULL,
+    access_token_expiry  INT          NOT NULL,
+    refresh_token_expiry INT          NOT NULL,
+    id_token_expiry      INT          NOT NULL,
+    id_token_claims      JSON         NOT NULL,
+    cookie_same_site     VARCHAR(20)  NOT NULL DEFAULT 'LAX',
+    cookie_domain        VARCHAR(256) NOT NULL,
+    cookie_path          VARCHAR(256) NOT NULL DEFAULT '/',
+    cookie_secure        BOOLEAN      NOT NULL DEFAULT FALSE,
+    cookie_http_only     BOOLEAN      NOT NULL DEFAULT TRUE,
+    access_token_claims  JSON         NOT NULL DEFAULT (JSON_ARRAY()),
+    created_at           TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at           TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_tenant_token_config FOREIGN KEY (tenant_id)
         REFERENCES tenant (id) ON DELETE CASCADE,
@@ -227,20 +227,20 @@ CREATE TABLE contact_verify_config
 
 CREATE TABLE oidc_provider_config
 (
-    tenant_id          CHAR(10)      NOT NULL,
-    provider_name      VARCHAR(50)   NOT NULL,
-    issuer             TEXT          NOT NULL,
-    jwks_url           TEXT          NOT NULL,
-    token_url          TEXT          NOT NULL,
-    client_id          VARCHAR(256)  NOT NULL,
-    client_secret      TEXT          NOT NULL,
-    redirect_uri       TEXT          NOT NULL,
-    client_auth_method VARCHAR(256)  NOT NULL,
-    is_ssl_enabled     BOOLEAN       NOT NULL DEFAULT TRUE,
-    user_identifier    VARCHAR(20)   NOT NULL DEFAULT 'email',
-    audience_claims    JSON          NOT NULL,
-    created_at         TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at         TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    tenant_id          CHAR(10)     NOT NULL,
+    provider_name      VARCHAR(50)  NOT NULL,
+    issuer             TEXT         NOT NULL,
+    jwks_url           TEXT         NOT NULL,
+    token_url          TEXT         NOT NULL,
+    client_id          VARCHAR(256) NOT NULL,
+    client_secret      TEXT         NOT NULL,
+    redirect_uri       TEXT         NOT NULL,
+    client_auth_method VARCHAR(256) NOT NULL,
+    is_ssl_enabled     BOOLEAN      NOT NULL DEFAULT TRUE,
+    user_identifier    VARCHAR(20)  NOT NULL DEFAULT 'email',
+    audience_claims    JSON         NOT NULL,
+    created_at         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_tenant_oidc_provider_config FOREIGN KEY (tenant_id)
         REFERENCES tenant (id) ON DELETE CASCADE,
     PRIMARY KEY `idx_tenant_provider` (`tenant_id`, `provider_name`)
@@ -250,21 +250,21 @@ CREATE TABLE oidc_provider_config
 
 CREATE TABLE user_flow_block
 (
-    tenant_id           CHAR(10)        NOT NULL,
-    user_identifier     VARCHAR(64)     NOT NULL,
-    flow_name           CHAR(20)        NOT NULL,
-    reason              VARCHAR(500),
-    unblocked_at        BIGINT,
-    is_active           BOOLEAN         NOT NULL DEFAULT TRUE,
-    created_at          TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at          TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    tenant_id       CHAR(10)    NOT NULL,
+    user_identifier VARCHAR(64) NOT NULL,
+    flow_name       CHAR(20)    NOT NULL,
+    reason          VARCHAR(500),
+    unblocked_at    BIGINT,
+    is_active       BOOLEAN     NOT NULL DEFAULT TRUE,
+    created_at      TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_tenant_user_flow_block FOREIGN KEY (tenant_id)
         REFERENCES tenant (id) ON DELETE CASCADE,
 
     PRIMARY KEY pk_tenant_user_flow (tenant_id, flow_name, user_identifier),
 
-        KEY idx_user_flow_block_tenant_user_flow (tenant_id, flow_name, user_identifier, unblocked_at)
+    KEY             idx_user_flow_block_tenant_user_flow (tenant_id, flow_name, user_identifier, unblocked_at)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
@@ -275,8 +275,8 @@ CREATE TABLE admin_config
     tenant_id  CHAR(10) PRIMARY KEY,
     username   VARCHAR(50) NOT NULL,
     password   VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_tenant_admin_config FOREIGN KEY (tenant_id)
         REFERENCES tenant (id) ON DELETE CASCADE,
@@ -299,35 +299,36 @@ CREATE TABLE scope
     created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     UNIQUE KEY `uniq_tenant_scope` (`tenant_id`, `name`),
-    KEY `idx_tenant_oidc` (`tenant_id`, `is_oidc`)
+    KEY          `idx_tenant_oidc` (`tenant_id`, `is_oidc`)
 ) ENGINE=InnoDB
   DEFAULT CHARSET=utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE oidc_config (
-    tenant_id CHAR(10) NOT NULL,
-    issuer VARCHAR(255) NOT NULL,
-    authorization_endpoint VARCHAR(255) NOT NULL,
-    token_endpoint VARCHAR(255) NOT NULL,
-    userinfo_endpoint VARCHAR(255) NOT NULL,
-    revocation_endpoint VARCHAR(255) NOT NULL,
-    jwks_uri VARCHAR(255) NOT NULL,
-    grant_types_supported JSON NOT NULL DEFAULT (JSON_ARRAY()),
-    response_types_supported JSON NOT NULL DEFAULT (JSON_ARRAY()),
-    subject_types_supported JSON NOT NULL DEFAULT (JSON_ARRAY()),
-    id_token_signing_alg_values_supported JSON NOT NULL DEFAULT (JSON_ARRAY()),
-    token_endpoint_auth_methods_supported JSON NOT NULL DEFAULT (JSON_ARRAY()),
-    login_page_uri VARCHAR(512) DEFAULT NULL,
-    consent_page_uri VARCHAR(512) DEFAULT NULL,
-    authorize_ttl INT DEFAULT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE oidc_config
+(
+    tenant_id                             CHAR(10)     NOT NULL,
+    issuer                                VARCHAR(255) NOT NULL,
+    authorization_endpoint                VARCHAR(255) NOT NULL,
+    token_endpoint                        VARCHAR(255) NOT NULL,
+    userinfo_endpoint                     VARCHAR(255) NOT NULL,
+    revocation_endpoint                   VARCHAR(255) NOT NULL,
+    jwks_uri                              VARCHAR(255) NOT NULL,
+    grant_types_supported                 JSON         NOT NULL DEFAULT (JSON_ARRAY()),
+    response_types_supported              JSON         NOT NULL DEFAULT (JSON_ARRAY()),
+    subject_types_supported               JSON         NOT NULL DEFAULT (JSON_ARRAY()),
+    id_token_signing_alg_values_supported JSON         NOT NULL DEFAULT (JSON_ARRAY()),
+    token_endpoint_auth_methods_supported JSON         NOT NULL DEFAULT (JSON_ARRAY()),
+    login_page_uri                        VARCHAR(512)          DEFAULT NULL,
+    consent_page_uri                      VARCHAR(512)          DEFAULT NULL,
+    authorize_ttl                         INT                   DEFAULT NULL,
+    updated_at                            TIMESTAMP             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at                            TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
 
     UNIQUE KEY `uniq_tenant_config` (`tenant_id`),
 
     CONSTRAINT `fk_oidc_config` FOREIGN KEY (`tenant_id`)
-        REFERENCES `tenant`(`id`) ON DELETE CASCADE
+        REFERENCES `tenant` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
@@ -383,14 +384,14 @@ CREATE TABLE consent
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (`tenant_id`, `client_id`, `user_id`, `scope`),
-    KEY `idx_tenant_scope` (`tenant_id`, `scope`),
+    KEY        `idx_tenant_scope` (`tenant_id`, `scope`),
 
     CONSTRAINT `fk_consent_tenant` FOREIGN KEY (`tenant_id`)
-        REFERENCES `tenant`(`id`) ON DELETE CASCADE,
+        REFERENCES `tenant` (`id`) ON DELETE CASCADE,
     CONSTRAINT `fk_consent_client` FOREIGN KEY (`tenant_id`, `client_id`)
-        REFERENCES `client`(`tenant_id`, `client_id`) ON DELETE CASCADE,
+        REFERENCES `client` (`tenant_id`, `client_id`) ON DELETE CASCADE,
     CONSTRAINT `fk_consent_scope` FOREIGN KEY (`tenant_id`, `scope`)
-        REFERENCES `scope`(`tenant_id`, `name`) ON DELETE CASCADE
+        REFERENCES `scope` (`tenant_id`, `name`) ON DELETE CASCADE
 ) ENGINE=InnoDB
   DEFAULT CHARSET=utf8mb4
   COLLATE=utf8mb4_0900_ai_ci;
@@ -404,7 +405,7 @@ CREATE TABLE oidc_refresh_token
     is_active         BOOLEAN                    NOT NULL DEFAULT TRUE,
     refresh_token     CHAR(32) COLLATE ascii_bin NOT NULL,
     refresh_token_exp BIGINT UNSIGNED NOT NULL,
-    scope             JSON                      NOT NULL DEFAULT (JSON_ARRAY()),
+    scope             JSON                       NOT NULL DEFAULT (JSON_ARRAY()),
     device_name       VARCHAR(256),
     ip                VARBINARY(16),
     created_at        TIMESTAMP                  NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -413,24 +414,26 @@ CREATE TABLE oidc_refresh_token
     PRIMARY KEY (id),
     KEY               `idx_oidc_refresh_token` (`tenant_id`, `client_id`, `refresh_token`, `is_active`, `refresh_token_exp`, `user_id`),
     KEY               `idx_oidc_refresh_token_user` (`tenant_id`, `user_id`),
-    CONSTRAINT `fk_oidc_refresh_token_tenant` FOREIGN KEY (`tenant_id`) REFERENCES `tenant`(`id`) ON DELETE CASCADE,
-    CONSTRAINT `fk_oidc_refresh_token_client` FOREIGN KEY (`tenant_id`, `client_id`) REFERENCES `client`(`tenant_id`, `client_id`) ON DELETE CASCADE
+    CONSTRAINT `fk_oidc_refresh_token_tenant` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_oidc_refresh_token_client` FOREIGN KEY (`tenant_id`, `client_id`) REFERENCES `client` (`tenant_id`, `client_id`) ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE guest_config (
-    tenant_id CHAR(10) PRIMARY KEY,
-    is_encrypted BOOLEAN NOT NULL DEFAULT true,
-    secret_key VARCHAR(16),
-    allowed_scopes JSON NOT NULL DEFAULT (JSON_ARRAY()),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+CREATE TABLE guest_config
+(
+    tenant_id      CHAR(10) PRIMARY KEY,
+    is_encrypted   BOOLEAN   NOT NULL DEFAULT true,
+    secret_key     VARCHAR(16)
+        COLLATE utf8mb4_0900_as_cs,
+    allowed_scopes JSON      NOT NULL DEFAULT (JSON_ARRAY()),
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    KEY `idx_guest` (`tenant_id`, `is_encrypted`),
+    KEY            `idx_guest` (`tenant_id`, `is_encrypted`),
     CONSTRAINT `fk_guest_tenant` FOREIGN KEY (`tenant_id`) REFERENCES `tenant` (`id`) ON DELETE CASCADE
 
-)ENGINE = InnoDB
+) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
