19037d3b83d97aa0e32c555f4d01ebf6d691f3bd
Animesh Agrawal <animesh.agrawal@dream11.com>
2025-10-03 12:17:58 +0530

    fix: validation annotation removed for social flows

--- src/main/java/com/dreamsportslabs/guardian/dto/request/v2/V2AuthGoogleRequestDto.java
+++ src/main/java/com/dreamsportslabs/guardian/dto/request/v2/V2AuthGoogleRequestDto.java
@@ -1,8 +1,8 @@
 package com.dreamsportslabs.guardian.dto.request.v2;
 
-import com.dreamsportslabs.guardian.validation.annotation.ValidV2AuthGoogleRequest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,11 +11,12 @@ import lombok.Setter;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
-@ValidV2AuthGoogleRequest
 public class V2AuthGoogleRequestDto extends V2AbstractAuthenticationRequestDto {
   @JsonProperty("id_token")
+  @NotBlank(message = "id_token is required")
   private String idToken;
 
   @JsonProperty("response_type")
+  @NotBlank(message = "response_type is required")
   private String responseType;
 }
--- src/main/java/com/dreamsportslabs/guardian/dto/request/v2/V2IdpConnectRequestDto.java
+++ src/main/java/com/dreamsportslabs/guardian/dto/request/v2/V2IdpConnectRequestDto.java
@@ -1,8 +1,8 @@
 package com.dreamsportslabs.guardian.dto.request.v2;
 
-import com.dreamsportslabs.guardian.validation.annotation.ValidV2IdpConnectRequest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,18 +11,21 @@ import lombok.Setter;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
-@ValidV2IdpConnectRequest
 public class V2IdpConnectRequestDto extends V2AbstractAuthenticationRequestDto {
   @JsonProperty("id_provider")
+  @NotBlank(message = "id_provider is required")
   private String idProvider;
 
   @JsonProperty("identifier")
+  @NotBlank(message = "identifier is required")
   private String identifier;
 
   @JsonProperty("identifier_type")
+  @NotBlank(message = "identifier_type is required")
   private String identifierType;
 
   @JsonProperty("response_type")
+  @NotBlank(message = "response_type is required")
   private String responseType;
 
   @JsonProperty("nonce")
