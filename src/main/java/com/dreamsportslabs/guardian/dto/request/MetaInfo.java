82fefa9dba57193f45a6dd05f3abd0ba39334943
Pujan Burad <buradpujan11@gmail.com>
2025-10-14 15:29:13 +0530

    fix: update handling of meta info for token creation (#94)

--- src/main/java/com/dreamsportslabs/guardian/dto/request/MetaInfo.java
+++ src/main/java/com/dreamsportslabs/guardian/dto/request/MetaInfo.java
@@ -1,13 +1,34 @@
 package com.dreamsportslabs.guardian.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class MetaInfo {
+  @JsonProperty("location")
   private String location;
+
+  @JsonProperty("device_name")
   private String deviceName;
+
+  @JsonProperty("ip")
   private String ip;
+
+  @JsonProperty("source")
   private String source;
+
+  // TODO: remove once v1 is deprecated
+  @JsonProperty("deviceName")
+  private String legacyDeviceName;
+
+  // TODO: remove once v1 is deprecated
+
+  public String getDeviceName() {
+    if (deviceName != null) {
+      return deviceName;
+    }
+    return legacyDeviceName;
+  }
 }
