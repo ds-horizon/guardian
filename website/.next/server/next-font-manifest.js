f338baf915f5020a66c2a457eae0108a6211ecfa
Subhash Kumar <subhash@dream11.com>
2025-11-26 01:58:31 +0530

    chore website files

---
+++ website/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/subhashkumar/dev/dream11/repo/guardian/website/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
